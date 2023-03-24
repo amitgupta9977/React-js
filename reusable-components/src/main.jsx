6e4f39e85bb740d1e3f1d8b58675fadf8ccd912d
pro <pro@pros-MacBook-Pro.local>
2023-03-24 08:55:55 +0530

    added

--- reusable-components/src/main.jsx
+++ reusable-components/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ButtonExample from './button';
-import BadgeExample from './badge';
+import ButtonExample from './ButtonExample.Layout';
+import BadgeExample from './BadgeExample.Layout';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
